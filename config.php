b990e2d6ee5d8a9585a126b279b313e1e4df9413
mike <michaelseiler@fuller.edu>
2014-12-16 17:29:36 -0800

    Added config file example; put the constant definitions wherever your own framework loads it's configuration.

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+// LDAP variables
+define("AD_HOST","ldaps://ad.example.com");
+define("AD_PORT","636");
+define("AD_USER","CN=admin user,CN=Users,DC=example,DC=com");
+define("AD_PASS","ExamplePassword123");
+define("AD_BASEDN","ou=users,DC=example,DC=com");
