aa58effc4b83b1b0a4b188f15e0eddf4c9dd21d5
Mike Seiler <cmikeseiler@users.noreply.github.com>
2016-07-04 13:32:42 -0700

    Update ActiveDirectory.php
    Updated link to further documentation.

--- ActiveDirectory/ActiveDirectory.php
+++ ActiveDirectory/ActiveDirectory.php
@@ -22,7 +22,7 @@ use Exception;
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * 
- * @link       http://michaelseiler.net/ActiveDirectory
+ * @link       http://michaelseiler.net/projects-code/activedirectory/
  */
 class ActiveDirectory
 {
